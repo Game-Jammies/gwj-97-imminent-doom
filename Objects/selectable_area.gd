e54da87d52032018cd2186a25e6c0bfede2d8971
Gunnar Dahl <73252935+gadahl@users.noreply.github.com>
2026-09-18 19:10:33 -0500

    selectable-area script

---
+++ Objects/selectable_area.gd
@@ -0,0 +1,19 @@
+class_name SelectableArea extends Area2D
+
+@onready var _sprite = $Sprite2D
+var can_select: bool = true
+var is_in_menu: bool = false
+var _is_hovering: bool = false
+
+func _ready() -> void:
+	mouse_entered.connect(_select)
+	mouse_exited.connect(_deselect)
+
+func _process(_delta) -> void:
+	if can_select and _is_hovering and not is_in_menu:
+		_sprite.frame = 1
+	else: 
+		_sprite.frame = 0
+
+func _select(): _is_hovering = true
+func _deselect(): _is_hovering = false
