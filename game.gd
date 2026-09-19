9d3874d982c01459cd132501fe6699fa0791931d
Julian Sahagun <43455717+Bluju@users.noreply.github.com>
2026-09-18 21:17:09 -0500

    add task dictionary and go_to_bed function

--- game.gd
+++ game.gd
@@ -7,6 +7,15 @@ extends Node2D
 @onready var bedroom := %Bedroom
 @onready var kitchen := %KitchenRoom
 
+## Dictionary with all of the task names, and whether they have been completed
+var tasks: Dictionary[String, bool] = {
+	"Trash" : false, # Take out the trash
+	"Chicken" : false, # Take the chicken out the fridge and put it in the sink
+	"FeedDog" : false, # Feed the dog
+	"WashDishes": false, # Wash the dishes
+	"TurnOffLight": false, # Turn off the bedroom lamp
+}
+
 
 func _ready() -> void:
 	kitchen.visible = false
@@ -21,6 +30,18 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var t = int(timer.time_left)
 	timer_label.text = "%d:%02d" % [t / 60, t % 60]
+
+# The player goes to bed
+func _on_player_sleeps() -> void:
+	var win = true
+	for task in tasks:
+		if not tasks[task]: # if task not complete
+			win = false
+			break
+	if win:
+		print("You survived!")
+	else:
+		print("You forgot something! You're grounded!")
 
 
 func _on_timer_timeout() -> void:
