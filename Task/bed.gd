cee8ce05f00229f0b610d2b15a636635bf3c17aa
Gunnar Dahl <73252935+gadahl@users.noreply.github.com>
2026-09-18 22:01:04 -0500

    made bed selectable (also print bedtime)

---
+++ Task/bed.gd
@@ -0,0 +1,11 @@
+extends SelectableArea
+
+# _sprite
+
+func _on_click() -> void: 
+	print("bedtime")
+	#var total_frames = _sprite.hframes * _sprite.vframes
+	#if _sprite.frame + 1 >= total_frames:
+		#_sprite.frame = 0
+	#else:
+		#_sprite.frame += 1
