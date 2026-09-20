df58197b71c91b3cbfd064ebf95285773993798e
Julian <sahaguj@pnw.edu>
2026-09-19 23:07:52 -0500

    add variety to note sprites

--- Task/note.gd
+++ Task/note.gd
@@ -1,10 +1,14 @@
 class_name Note extends SelectableArea
 
 signal note_found(message: String)
-
+@export var sprite_frame: int = 0
 @export var message: String = "Default"
+@onready var sprite = $Sprite2D
 
 func _on_click() -> void:
 	hide()
 	note_found.emit(message)
-	print("Note")
+
+func _ready() -> void: 
+	super()
+	sprite.frame = sprite_frame
