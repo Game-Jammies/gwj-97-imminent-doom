195ed0711e60d5c31f268d0ae417ccc3cbe50588
volunteer46 <volunteerdoors46@gmail.com>
2026-09-20 14:36:08 -0500

    add cutscenes

--- game.gd
+++ game.gd
@@ -7,6 +7,10 @@ extends Node2D
 @onready var bedroom := %Bedroom
 @onready var kitchen := %KitchenRoom
 @onready var bedtime_popup: PopupPanel = %BedtimePopup
+@onready var opening_cut:= %OpeningCut
+@onready var win_cut:= %WinCut
+@onready var lose_cut:= %LoseCut
+
 
 ## A list containing all the selectable objects
 var selectable_areas: Array[SelectableArea] = []
@@ -20,6 +24,9 @@ var tasks: Dictionary[String, bool] = {
 	"TurnOffLight": false, # Turn off the bedroom lamp
 }
 
+
+
+
 func finish_trash(): tasks["Trash"] = true
 func finish_chicken(): tasks["Chicken"] = true
 func finish_feed_dog(): tasks["FeedDog"] = true
@@ -28,6 +35,10 @@ func finish_light(): tasks["TurnOffLight"] = true
 
 
 func _ready() -> void:
+	opening_cut.play()
+	
+func _on_video_stream_player_finished():
+	opening_cut.visible = false
 	kitchen.visible = false
 	go_to_bedroom_button.visible = false
 	bedroom.visible = true
@@ -65,9 +76,9 @@ func _on_player_sleeps() -> void:
 			win = false
 			break
 	if win:
-		print("You survived!")
+		win_cut.play()
 	else:
-		print("You forgot something! You're grounded!")
+		lose_cut.play()
 
 
 func _on_timer_timeout() -> void:
